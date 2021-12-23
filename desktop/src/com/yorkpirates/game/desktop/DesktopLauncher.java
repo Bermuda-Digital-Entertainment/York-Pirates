f45a22309ae55400a400be9a35ba8c978361fc80
Joseph-S12 <jjsisson14@gmail.com>
2021-12-23 13:41:53 +0000

    Removed Default Assets
    Got rid of the default assets.
    Removed the code for image rendering (temporarily)

--- desktop/src/com/yorkpirates/game/desktop/DesktopLauncher.java
+++ desktop/src/com/yorkpirates/game/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.yorkpirates.game.YorkPirates;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("York Pirates");
+    config.setMaximized(true);
 		new Lwjgl3Application(new YorkPirates(), config);
 	}
 }
