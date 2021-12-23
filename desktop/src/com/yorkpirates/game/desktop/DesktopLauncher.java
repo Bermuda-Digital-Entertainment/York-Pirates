f14020d15316ea335eb7a443d4c3e4d2d837f0cb
Joseph-S12 <80111134+Joseph-S12@users.noreply.github.com>
2021-12-23 13:02:34 +0000

    Initial commit of base code
    Added the base game code to the repository, as generated by libgdx's setup program, with alterations to:
    Gradle (changed version from v6 to v7)
    lwjgl (changed from v2 to v3)

---
+++ desktop/src/com/yorkpirates/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.yorkpirates.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.yorkpirates.game.YorkPirates;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new YorkPirates(), config);
+	}
+}
