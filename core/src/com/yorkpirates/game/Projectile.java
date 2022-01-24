155434c064d1cda3e8358b4e99160a34c897609a
Wardstone111 <jjsisson14@gmail.com>
2022-01-24 13:06:11 +0000

    Create Projectile.java

---
+++ core/src/com/yorkpirates/game/Projectile.java
@@ -0,0 +1,18 @@
+package com.yorkpirates.game;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.Texture;
+
+public class Projectile extends Sprite {
+  protected Integer speedX;
+  protected Integer speedY;
+  protected Integer range;
+  protected Integer originX;
+  protected Integer originY;
+  protected Integer damage;
+  protected Integer speed;
+
+  public Projectile (Integer srcX, Integer srcY, Integer dirX, Integer dirY, Integer initRange) {
+    
+  }
+}
