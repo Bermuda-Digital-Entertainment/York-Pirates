bd681eb9f25ae1ca086a43f919b39042c86a0da5
Wardstone111 <jjsisson14@gmail.com>
2022-01-31 21:33:56 +0000

    Added javadocs to Boat Class

--- core/src/com/yorkpirates/game/Boat.java
+++ core/src/com/yorkpirates/game/Boat.java
@@ -9,7 +9,7 @@ import com.badlogic.gdx.math.Rectangle;
 
 
 /**
-* Boat Class. Implements a sprite with additional Properties for Health and boat Level. It can be an AI boat or a player
+* Boat Class. Implements a sprite with additional Properties for Health, boat Level, etc. It can be an AI boat or a player
 */
 public class Boat extends Sprite {
   private Float x;
@@ -22,12 +22,16 @@ public class Boat extends Sprite {
   public float firingRate = 1f;
   public Float projectileDamage = 10f;
   public Float projectileRange = 500f;
-  public Float projectileSpeed;
+  public Float projectileSpeed = 60f;
   public Integer lastDirectionMoved; //0 is up, 1 is right, 2 is down, 3 is left
 	public float lastShotTime;
   public final int ID;
 
-  
+
+  /**
+  * Constructor method for Boar. It simply gives the boat a unique ID
+  * @param boatID int Passes a unique ID to the boat (to prevent an object hitting itself with a cannon ball)
+  */
   public Boat(int boatID){
     lastShotTime=6;
     health = 50f;
@@ -35,6 +39,12 @@ public class Boat extends Sprite {
     this.ID=boatID;
   }
 
+  /**
+  * Method checking if the boat has collided with another object
+  * @param collisionColleges A list of all the colleges the boat could collide with
+  * @param collisionBoats A list of all the boats the boat could collide with
+  * @return If the boat is overlapping another object in either list, return true
+  */
   public Boolean collides(ArrayList<College> collisionColleges, ArrayList<Boat> collisionBoats){
     Boolean collisionExists = false;
     for (Integer i=0; i<collisionColleges.size(); i++) {
@@ -46,6 +56,11 @@ public class Boat extends Sprite {
     return collisionExists;
   }
 
+  /**
+  * Method checking if the boat has collided with a cannon ball
+  * @param collisionProjectile A bullet that is to be checked
+  * @return If the boat is overlapping another a bullet, return true
+  */
   public Boolean isHit(Bullet collisionProjectile) {
     Rectangle testRectangle = new Rectangle(collisionProjectile.x,collisionProjectile.y,16,16);
     Boolean hit = false;
@@ -57,6 +72,10 @@ public class Boat extends Sprite {
     return hit;
   }
 
+  /**
+  * A method creating a bullet
+  * @return A bullet fired forwards relative to the boat
+  */
   public Bullet fire() {
     Bullet cannonBall;
     switch (lastDirectionMoved) {
@@ -77,6 +96,10 @@ public class Boat extends Sprite {
     return cannonBall;
   }
 
+  /**
+  * A method to get the current texture
+  * @return The current texture of the boat (or that the boat should have)
+  */
   public Texture getTexture(){
     Texture texture;
     switch (lastDirectionMoved){
@@ -96,26 +119,49 @@ public class Boat extends Sprite {
   return texture;
   }
 
+  /**
+  * A method to check if the boat can fire yet
+  * @return If more time has passed since the previous shot than firingRate, return true
+  */
   public Boolean canFire(){
     return (lastShotTime > firingRate);
   }
 
+  /**
+  * Adds deltaTime to the lastShotTime
+  * @param time Time that has ellapsed between two renders of the screen.
+  */
   public void addTime(float time){
     lastShotTime+=time;
   }
 
+  /**
+  * Resets lastShotTime to 0
+  */
   public void resetFire(){
     lastShotTime=0;
   }
 
+  /**
+  * Returns the speed at which a bullet can be fireds
+  * @return projectileSpeed
+  */
   public float getBulletSpeed(){
-    return speed;
+    return projectileSpeed;
   }
 
+  /**
+  * Returns the proportion of the maximum health left of the boat
+  * @return health/maxhealth
+  */
   public float health(){
     return health/maxHealth;
   }
 
+  /**
+  * Returns whether or not the boat has been destroyed
+  * @return If the health of the boat is less than or equal to 0, return true
+  */
   public Boolean isDestroyed(){
     if (health<=0)
       return true;
